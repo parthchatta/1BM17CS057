7d6400c030b0fecbcd7cc8c41128040a6612eabd
parth chatta <49511465+parthchatta@users.noreply.github.com>
2019-10-15 02:24:54 +0530

    Add files via upload

---
+++ 7B.py
@@ -0,0 +1,13 @@
+def disp():
+    print(" ",end="")
+    for j in range(3):
+        for i in range(3):
+            print("-",end="")
+        print("",end=" ")
+    print(" ")
+    for i in range(4):
+        print("| ",end="  ")
+    print("")
+for i in range(3):
+    disp()
+
